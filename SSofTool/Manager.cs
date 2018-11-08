9d049f2b2774acd8ea2677070e96ec2eea41056b
Pedro Mela <NB24727@novabase.pt>
2018-11-08 11:27:57 +0000

    mov fixed

--- SSofTool/Manager.cs
+++ SSofTool/Manager.cs
@@ -87,10 +87,10 @@ namespace SSofTool
 
 									//Console.WriteLine("RSP: " + instr.args[1].ToString());
 									int intValue = Convert.ToInt32(instr.args[1].ToString(), 16);
-                                    Frame frame = frames.First();
-                                    frame.end = pointer + intValue;
-									//Frame frame = new Frame(pointer, pointer + intValue);
-									//frames.Push(frame);
+                                    //Frame frame = frames.First();
+                                    //frame.end = pointer + intValue;
+									Frame frame = new Frame(pointer, pointer + intValue);
+									frames.Push(frame);
 									for (int i = 0; i < intValue; i++)
 									{
 										//s.Push('0');
@@ -113,7 +113,7 @@ namespace SSofTool
                                     Frame frame = new Frame();
                                     frame.start = pointer;
                                     frames.Push(frame);
-									/*int len = instr.address.Length;
+									int len = instr.address.Length;
 									while (len != 8)
 									{
 										s.Push('0');
@@ -126,7 +126,7 @@ namespace SSofTool
 										s.Push(c);
 										stack.Add(pointer,c);
 										pointer++;
-									} */
+									} 
 								}
 							}
 							break;
@@ -153,15 +153,15 @@ namespace SSofTool
 												if (!string.IsNullOrEmpty(instr.args[1].ToString()))
 												{
 													Frame frame = frames.First();
-													int i = frame.start;
-
+													int i = frame.start + intValue;
+													Console.WriteLine("i : " + i);
 													foreach (char c in instr.args[1].ToString().Substring(2))
 													{
 														stack[i] = c;
-														i++;
+														i--;
 													}
 												}
-												//Console.WriteLine("WORD PTR " + intValue);
+												Console.WriteLine("WORD PTR " + intValue);
 											}
 										}
 									}
@@ -176,63 +176,63 @@ namespace SSofTool
 
 							break;
 						case "call":
-                            if (instr.args.Length < 1)
-                            {
-                                Console.WriteLine("This call makes no sense");
-                            }
-                            Console.WriteLine("{0} {1} {2}", instr.pos, instr.args[0], instr.args[1]);
+         //                   if (instr.args.Length < 1)
+         //                   {
+         //                       Console.WriteLine("This call makes no sense");
+         //                   }
+         //                   Console.WriteLine("{0} {1} {2}", instr.pos, instr.args[0], instr.args[1]);
 
 
-                            switch (instr.args[0].ToString())
-                            {
-                                //case for dangerous function calls
-                                case "<fgets@plt>":
-                                    Console.WriteLine("{0} {1} {2}", instr.pos, instr.args[0], instr.args[1]);
+         //                   switch (instr.args[0].ToString())
+         //                   {
+         //                       //case for dangerous function calls
+         //                       case "<fgets@plt>":
+         //                           Console.WriteLine("{0} {1} {2}", instr.pos, instr.args[0], instr.args[1]);
 
-                                    //fgets is dangerous, there's 3 arguments  that are put in registers before calling an fgets
-                                    //the buffer (LEA'd into register)
-                                    //the buff_len (mov'd into register)
-                                    //the stdinstream (This is accessed via mov [rip-"code"])
-                                    string buffstart;
-                                    string bufflen;
-                                    string[] split;
-                                    string rip;
-                                    string register;
-									Console.WriteLine("CALL:");
-                                    int instrcounter = instr.pos - 4;
-                                    Instruction instrstdin = f.GetInstruction(instrcounter);
+         //                           //fgets is dangerous, there's 3 arguments  that are put in registers before calling an fgets
+         //                           //the buffer (LEA'd into register)
+         //                           //the buff_len (mov'd into register)
+         //                           //the stdinstream (This is accessed via mov [rip-"code"])
+         //                           string buffstart;
+         //                           string bufflen;
+         //                           string[] split;
+         //                           string rip;
+         //                           string register;
+									//Console.WriteLine("CALL:");
+         //                           int instrcounter = instr.pos - 4;
+         //                           Instruction instrstdin = f.GetInstruction(instrcounter);
 
-                                    rip = instrstdin.args[1].ToString();
-                                    split = rip.Split(' ');
-                                    if (split[2].Equals("[rip+0x200b03]")) //ponteiro p stdin q vai ser passado ao fgets, uma call ao fgets faz smpr isto nesta ordem
-                                    {
-                                        Console.WriteLine("{0} {1} {2}", instrstdin.pos, instrstdin.args[0], instrstdin.args[1]);
-                                        instrcounter++;
-                                        Instruction instrlea = f.GetInstruction(instrcounter);
-                                        if (instrlea.op.Equals("lea"))
-                                        {
-                                            register = instrlea.args[0].ToString();
-                                            buffstart = instrlea.args[1].ToString(); // ponteiro pa variavel buffer na stack é guardado num registo q vai ser passado ao fgets
-                                            instrcounter++;
-                                            Instruction instrbufflen = f.GetInstruction(instrcounter);
-                                            if (instrbufflen.op.Equals("mov") && instrbufflen.args[0].ToString().Equals("esi"))
-                                            {
-                                                bufflen = instrbufflen.args[1].ToString(); // o valor do buff_len do fgets e posto num registo
-                                                instrcounter++;
-                                                Instruction instrmov = f.GetInstruction(instrcounter++);
-                                                if (instrmov.op.Equals("mov") && instrmov.args[1].ToString().Equals(register)) // o ponteiro para o buffer que foi colocado num registo é posto noutro registo conhecido ao fgets
-                                                {
-                                                    Console.WriteLine("fgets chamou, guardámos o bufflen, agr vamos ver se pode existir acesso outofbound, ou um overflow numa variável da frame");
-                                                    List<Variable> variablelist = f.getVariables();
+         //                           rip = instrstdin.args[1].ToString();
+         //                           split = rip.Split(' ');
+         //                           if (split[2].Equals("[rip+0x200b03]")) //ponteiro p stdin q vai ser passado ao fgets, uma call ao fgets faz smpr isto nesta ordem
+         //                           {
+         //                               Console.WriteLine("{0} {1} {2}", instrstdin.pos, instrstdin.args[0], instrstdin.args[1]);
+         //                               instrcounter++;
+         //                               Instruction instrlea = f.GetInstruction(instrcounter);
+         //                               if (instrlea.op.Equals("lea"))
+         //                               {
+         //                                   register = instrlea.args[0].ToString();
+         //                                   buffstart = instrlea.args[1].ToString(); // ponteiro pa variavel buffer na stack é guardado num registo q vai ser passado ao fgets
+         //                                   instrcounter++;
+         //                                   Instruction instrbufflen = f.GetInstruction(instrcounter);
+         //                                   if (instrbufflen.op.Equals("mov") && instrbufflen.args[0].ToString().Equals("esi"))
+         //                                   {
+         //                                       bufflen = instrbufflen.args[1].ToString(); // o valor do buff_len do fgets e posto num registo
+         //                                       instrcounter++;
+         //                                       Instruction instrmov = f.GetInstruction(instrcounter++);
+         //                                       if (instrmov.op.Equals("mov") && instrmov.args[1].ToString().Equals(register)) // o ponteiro para o buffer que foi colocado num registo é posto noutro registo conhecido ao fgets
+         //                                       {
+         //                                           Console.WriteLine("fgets chamou, guardámos o bufflen, agr vamos ver se pode existir acesso outofbound, ou um overflow numa variável da frame");
+         //                                           List<Variable> variablelist = f.getVariables();
 
 
-                                                }
-                                            }
-                                        }
-                                    }
-                                    break;
+         //                                       }
+         //                                   }
+         //                               }
+         //                           }
+                            //        break;
 
-                            }
+                            //}
                             break;
 
                             
